2666f7a6e648624492d5e5d6e7e9815fac7c0b88
Josh Schonstal <josh@schonstal.org>
2017-01-22 12:23:13 -0800

    Basic camera movement

---
+++ Assets/MoveCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour {
+  public float speed = 0.1f;
+  void Start() {
+  }
+
+  void Update() {
+    if(Camera.current == null) { return; }
+
+    float xAxisValue = Input.GetAxis("Horizontal") * speed;
+    float yAxisValue = Input.GetAxis("Vertical") * speed;
+    Camera.current.transform.Translate(new Vector3(xAxisValue, yAxisValue, 0));
+  }
+}
