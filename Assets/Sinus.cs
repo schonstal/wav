b57ffd42b95a2079f645908244df00dd33b68f90
Josh Schonstal <josh@schonstal.org>
2017-01-21 21:09:51 -0800

    testing pendulum

--- Assets/Sinus.cs
+++ Assets/Sinus.cs
@@ -7,11 +7,15 @@ public class Sinus : MonoBehaviour
   public double frequency = 20;
   public double sampleRate = 48000;
   public double changeRate = 0.1;
+
   public bool scale = false;
+
+  public bool swing = false;
   public bool oscillate = false;
   public float oscillationStart = 80f;
   public float oscillationEnd = 100f;
   public float oscillationRate = 1f;
+  public float oscillationOffset = 0f;
 
   private float oscTheta = 0f;
   private double tau = 2 * Math.PI;
@@ -39,7 +43,7 @@ public class Sinus : MonoBehaviour
   void Update() {
     if (oscillate) {
       oscTheta += (float)(Time.deltaTime * tau * oscillationRate);
-      float oscPosition = ((float)Math.Sin(oscTheta) + 1) / 2f;
+      float oscPosition = ((float)Math.Sin(oscTheta + oscillationOffset) + 1) / 2f;
 
       frequency = Mathf.Lerp(
         oscillationEnd,
@@ -47,10 +51,16 @@ public class Sinus : MonoBehaviour
         oscPosition
       );
 
-      transform.localScale = new Vector3(
-        Mathf.Lerp(0, 6, oscPosition),
-        Mathf.Lerp(0, 6, oscPosition)
-      );
+      if (scale) {
+        transform.localScale = new Vector3(
+          Mathf.Lerp(0, 6, oscPosition),
+          Mathf.Lerp(0, 6, oscPosition)
+        );
+      }
+
+      if (swing) {
+        transform.eulerAngles = new Vector3(0, 0, (float)Math.Sin(oscTheta + oscillationOffset) * 60f);
+      }
     }
   }
 }
