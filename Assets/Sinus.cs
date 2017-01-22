c67d7434848d1b3735052bbd48786256669efc76
Josh Schonstal <josh@schonstal.org>
2017-01-21 19:54:21 -0800

    add oscillation

--- Assets/Sinus.cs
+++ Assets/Sinus.cs
@@ -8,16 +8,23 @@ public class Sinus : MonoBehaviour
   public double sampleRate = 48000;
   public double changeRate = 0.1;
   public bool scale = false;
+  public bool oscillate = false;
+  public float oscillationStart = 80f;
+  public float oscillationEnd = 100f;
+  public float oscillationRate = 1f;
+
+  private float oscTheta = 0f;
+  private double tau = 2 * Math.PI;
 
   private double theta;
   private float amplitude;
 
   void OnAudioFilterRead(float[] data, int channels) {
-    double sinAmt = frequency * 2 * Math.PI / sampleRate;
+    double sinAmt = frequency * tau / sampleRate;
 
     for (var i = 0; i < data.Length; i = i + channels) {
       theta = theta + sinAmt;
-      if (theta > 2 * Math.PI) {
+      if (theta > tau) {
         theta = 0;
       }
 
@@ -30,12 +37,20 @@ public class Sinus : MonoBehaviour
   }
 
   void Update() {
-    if (Input.GetKeyDown("space")) {
-      frequency += changeRate;
-    }
+    if (oscillate) {
+      oscTheta += (float)(Time.deltaTime * tau * oscillationRate);
+      float oscPosition = ((float)Math.Sin(oscTheta) + 1) / 2f;
 
-    if (scale) {
-      transform.localScale = new Vector3(6 * amplitude, 6 * amplitude, 1);
+      frequency = Mathf.Lerp(
+        oscillationEnd,
+        oscillationStart,
+        oscPosition
+      );
+
+      transform.localScale = new Vector3(
+        Mathf.Lerp(0, 6, oscPosition),
+        Mathf.Lerp(0, 6, oscPosition)
+      );
     }
   }
 }
